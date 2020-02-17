4746a17c2c68e83170b6b5707ba05bb2b34499c9
Satish Maurya <satish.maurya@lapsun21.successivetech.com>
2020-02-17 17:20:43 +0530

    Updates

---
+++ precache-manifest.0ecb3d661886c99774e3e46bb6af2ca2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826937514cee0435d392e1597d2479fb",
+    "url": "/react-user-permission-task-client/index.html"
+  },
+  {
+    "revision": "d975ba55619c7f8bc2d9",
+    "url": "/react-user-permission-task-client/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b865d51733d0653aff4d",
+    "url": "/react-user-permission-task-client/static/js/2.3cd49b82.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/react-user-permission-task-client/static/js/2.3cd49b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d975ba55619c7f8bc2d9",
+    "url": "/react-user-permission-task-client/static/js/main.b808f058.chunk.js"
+  },
+  {
+    "revision": "2398f68527f7911d9eae",
+    "url": "/react-user-permission-task-client/static/js/runtime-main.a076a3e8.js"
+  },
+  {
+    "revision": "92bd2c2ae088a44e24ae0fe4480fa83b",
+    "url": "/react-user-permission-task-client/static/media/losted.92bd2c2a.png"
+  }
+]);
