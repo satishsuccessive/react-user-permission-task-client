55b7e265a22f81223364d226facc8e12725a5b20
Satish Maurya <satish.maurya@lapsun21.successivetech.com>
2020-02-17 19:56:00 +0530

    Updates

---
+++ precache-manifest.97283d245c57981a9cc12b8d8c2fc54d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e457bc5331b97b247571b1ff395c29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c63269db9c9a48c37676",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5cda08784d5bacc3b7d5",
+    "url": "/static/js/2.80b811ae.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/static/js/2.80b811ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c63269db9c9a48c37676",
+    "url": "/static/js/main.c40e65d1.chunk.js"
+  },
+  {
+    "revision": "782669045abcc38adaf2",
+    "url": "/static/js/runtime-main.cfe419c5.js"
+  },
+  {
+    "revision": "92bd2c2ae088a44e24ae0fe4480fa83b",
+    "url": "/static/media/losted.92bd2c2a.png"
+  }
+]);
